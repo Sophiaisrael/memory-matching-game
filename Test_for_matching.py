6ba6277eb42c2501185f98a27bd75e83c3496cef
Sophiaisrael <Sisrael@tulane.edu>
2026-04-22 15:08:15 -0500

    Update tests to match current game logic

--- Test_for_matching.py
+++ Test_for_matching.py
@@ -1,48 +1,40 @@
 import unittest
-from memory_game import create_board, check_match, mark_matched
+from memory_game import create_board, all_matched
 
 
 class TestMemoryGame(unittest.TestCase):
 
     def test_create_board_size(self):
-        board = create_board()
+        board = create_board(2)
         self.assertEqual(len(board), 4)
         for row in board:
             self.assertEqual(len(row), 4)
 
-    def test_board_has_pairs(self):
-        board = create_board()
-        values = []
-        for row in board:
-            for value, status in row:
-                values.append(value)
+    def test_values_match_size_2(self):
+        board = create_board(2)
+        values = [v for row in board for v, _ in row]
 
         self.assertEqual(len(values), 16)
 
-        for number in set(values):
-            self.assertEqual(values.count(number), 2)
+        for val in set(values):
+            self.assertEqual(values.count(val), 2)
 
-    def test_check_match_true(self):
-        board = [
-            [(1, "hidden"), (2, "hidden")],
-            [(1, "hidden"), (3, "hidden")]
-        ]
-        self.assertTrue(check_match(board, (0, 0), (1, 0)))
+    def test_values_match_size_4(self):
+        board = create_board(4)
+        values = [v for row in board for v, _ in row]
 
-    def test_check_match_false(self):
-        board = [
-            [(1, "hidden"), (2, "hidden")],
-            [(3, "hidden"), (4, "hidden")]
-        ]
-        self.assertFalse(check_match(board, (0, 0), (0, 1)))
+        self.assertEqual(len(values), 16)
 
-    def test_mark_matched(self):
-        board = [
-            [(1, "hidden"), (1, "hidden")]
-        ]
-        mark_matched(board, (0, 0), (0, 1))
-        self.assertEqual(board[0][0][1], "matched")
-        self.assertEqual(board[0][1][1], "matched")
+        for val in set(values):
+            self.assertEqual(values.count(val), 4)
+
+    def test_all_matched_false(self):
+        board = create_board(2)
+        self.assertFalse(all_matched(board))
+
+    def test_all_matched_true(self):
+        board = [[(1, "matched") for _ in range(4)] for _ in range(4)]
+        self.assertTrue(all_matched(board))
 
 
 if __name__ == "__main__":
